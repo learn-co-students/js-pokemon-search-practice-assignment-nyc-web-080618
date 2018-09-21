aa05f0f21285ac31bdfb36c5cd76e8d1daa5a0fc
Andrew Cohn <andrewcohn0@gmail.com>
2018-09-21 17:35:56 -0400

    SOLUTION

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log(POKEMON)
+document.addEventListener('DOMContentLoaded', () => {
+  // grab the DOM elements we care about:
+  const divToAppendPokemon = document.querySelector('#poke-container')
+  const userSearchInputField = document.querySelector('#pokemon-search-input')
+
+  console.log(POKEMON) //log our data
+  // create new pokemon as soon as our app mounts:
+  POKEMON.forEach(pokeData => new Pokemon(pokeData))
+  // initially render ALL pokemon
+  divToAppendPokemon.innerHTML = Pokemon.renderAll()
+
+  userSearchInputField.addEventListener('input', event => {
+    const userSearchTerm = event.target.value
+    Pokemon.filterPokemonBasedOnSearchTerm(userSearchTerm)
+    divToAppendPokemon.innerHTML = Pokemon.renderAll()
+  })
+  // DOM events will bubble up the tree
+  // a parent node will know about ALL EVENTS that happen to its children
+  // we can listen for clicks on the outermost PARENT NODE and respond accordingly
+  divToAppendPokemon.addEventListener('click', event => {
+    // if the user clicked a button
+    if (event.target.nodeName === 'BUTTON') {
+      const clickedPokemonName = event.target.dataset.name
+
+      Pokemon.flipPokemonImage(clickedPokemonName)
+      divToAppendPokemon.innerHTML = Pokemon.renderAll()
+    }
+  })
 })
---
+++ src/pokemon.js
@@ -0,0 +1,59 @@
+const Pokemon = (() => {
+  // array to store ALL pokemon
+  const allPokemon = []
+  let filteredListOfPokemon = null
+
+  return class Pokemon {
+    constructor(attributesObject) {
+      this.id = attributesObject.order
+      this.name = attributesObject.name
+      this.abilities = attributesObject.abilities
+      this.sprites = attributesObject.sprites
+      this.flippedCard = false
+      // add each newly created pokemon "instance" to our allPokemon array
+      allPokemon.push(this)
+    }
+
+    static renderAll() {
+      //default to all pokemon if no arg is passed
+      //static is like a "class method": Pokemon.renderAll()
+      // return a large string of HTML that we can use to set innerHTML when rendering and re-rendering all pokemon
+      if (filteredListOfPokemon) {
+        return filteredListOfPokemon.map(pokeInstance => pokeInstance.render()).join('')
+      } else {
+        return allPokemon.map(pokeInstance => pokeInstance.render()).join('')
+      }
+    }
+
+    static filterPokemonBasedOnSearchTerm(userPokeNameToSearch) {
+      filteredListOfPokemon = allPokemon.filter(pokeInstance =>
+        pokeInstance.name.includes(userPokeNameToSearch.toLowerCase())
+      )
+    }
+
+    static flipPokemonImage(pokeName) {
+      // find the pokemon that the user clicked by name
+      const clickedPokemon = allPokemon.find(pokeInstance => pokeInstance.name === pokeName)
+      // toggle flipped from true to false
+      clickedPokemon.flippedCard = !clickedPokemon.flippedCard
+    }
+
+    render() {
+      // "instance" method on an individual pokemon: pikachu.render()
+      // since the EXECUTION CONTEXT of this method is pikachu, `this` will be that pokemon
+      return `<div class="pokemon-container">
+          <div style="width:230px;margin:10px;background:#fecd2f;color:#2d72fc">
+            <h1 class="center-text">${this.name}</h1>
+            <div style="width:239px;margin:auto">
+              <div style="width:96px;margin:auto">
+                <img src=${this.flippedCard ? this.sprites.back : this.sprites.front}>
+              </div>
+            </div>
+            <button data-name="${this.name}" style="padding:10px;" class="center-text flip-image">
+              FlipIt™️
+            </button>
+          </div>
+        </div>`
+    }
+  }
+})()
